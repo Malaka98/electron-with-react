44e180a68b34a7286f7a798ba390564b10dfca01
Malaka98 <malaka.aruna14413@gmail.com>
2023-02-13 18:23:36 +0530

    feat

--- webpack.config.js
+++ webpack.config.js
@@ -15,9 +15,13 @@ module.exports = {
                     loader: 'babel-loader',
                     options: {
                         presets: ['@babel/preset-env', '@babel/preset-react']
-                    }
-                }
-            }
+                    },
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader', 'postcss-loader'],
+            },
         ]
     }
 };
